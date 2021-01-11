b3bec75a50148994de6e3f97612d3040f89cfb1e
Oliver1029 <49137131+Oliver1029@users.noreply.github.com>
2021-01-11 18:52:32 +0800

    first edition

---
+++ Anniversary/Anniversary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Anniversary.Startup))]
+namespace Anniversary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
